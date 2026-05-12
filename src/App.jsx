ad731baab092d10bfb402fc55575709e6d6547ee
Alejand5672 <her241424@uvg.edu.gt>
2026-05-12 10:27:33 -0600

    Nivel 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Pomodoro from "./Pomodoro";
+import './App.css'
 
 function App() {
   return <Pomodoro />;
--- src/Pomodoro.jsx
+++ src/Pomodoro.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect, useRef } from 'react';
 
+const WORK_TIME = 1500; 
+const BREAK_TIME = 300; 
 function Pomodoro() {
   // TODO 1: Declara los estados timeLeft e isRunning
-  const [timeLeft, setTimeLeft] = useState(1500);
+  const [timeLeft, setTimeLeft] = useState(WORK_TIME);
   const [isRunning, setIsRunning] = useState(false);
+  const [mode, setMode] = useState("work");
+  const [sessions, setSessions] = useState([]);
+  
+
 
   // TODO 2: Declara intervalRef con useRef
   const intervalRef = useRef(null);
@@ -35,6 +41,29 @@ function Pomodoro() {
     return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
     };
 
+  useEffect(() => {
+    if (timeLeft === 0) {
+
+        if (mode === "work") {
+        const newSession = {
+            id: Date.now(),
+            type: "work",
+            duration: WORK_TIME,
+            completedAt: new Date()
+        };
+
+        setSessions((prev) => [...prev, newSession]);
+        }
+
+        const newMode = mode === "work" ? "break" : "work";
+        setMode(newMode);
+
+        setTimeLeft(newMode === "work" ? WORK_TIME : BREAK_TIME);
+
+        setIsRunning(true);
+    }
+    }, [timeLeft]);  
+
 
   // TODO 5: Funciones toggleTimer y resetTimer
   const toggleTimer = () => {
@@ -42,21 +71,44 @@ function Pomodoro() {
     };
 
     const resetTimer = () => {
-    setIsRunning(false);
-    setTimeLeft(1500);
+      setIsRunning(false);
+      setTimeLeft(WORK_TIME);
+      setMode("work");
+      setSessions([]);
     };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-    <h1>{formatTime(timeLeft)}</h1>
+    <div className="container">
+    <div className="card">
 
-    <button onClick={toggleTimer}>
-      {isRunning ? "Pausar" : "Iniciar"}
-    </button>
+      <div className="mode">
+        {mode === "work" ? "Trabajo" : "Descanso"}
+      </div>
 
-    <button onClick={resetTimer} style={{ marginLeft: "10px" }}>
-      Reiniciar
-    </button>
+      <div className="timer">
+        {formatTime(timeLeft)}
+      </div>
+
+      <div className="buttons">
+        <button className="start" onClick={toggleTimer}>
+          {isRunning ? "Pausar" : "Iniciar"}
+        </button>
+
+        <button className="reset" onClick={resetTimer}>
+          Reiniciar
+        </button>
+      </div>
+
+      <ul className="sessions">
+        {sessions.map((session, index) => (
+          <li key={session.id}>
+            #{index + 1} - {formatTime(session.duration)} -{" "}
+            {session.completedAt.toLocaleTimeString()}
+          </li>
+        ))}
+      </ul>
+
+    </div>
   </div>
   );
 }
